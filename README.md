6b8ad26ed2479fec61f245ddf82f510a1ff3ede4
alenaodinets <alena.odinecz2013@yandex.ru>
2024-07-27 17:47:15 +0400

    hw 27.2

---
+++ README.md
@@ -0,0 +1,9 @@
+Для запуска проекта:
+
+1. Установите Docker и docker-compose
+
+2. Склонируйте репозиторий git@github.com:alenaodinets/drf_project_hw.git
+
+3. Перейдите в папку с проектом
+
+4. Введите команду: sudo docker-compose up -d --build
--- config/settings.py
+++ config/settings.py
@@ -101,11 +101,11 @@ SIMPLE_JWT = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+        "POSTGRES_DB": os.getenv("POSTGRES_DB"),
+        "POSTGRES_USER": os.getenv("POSTGRES_USER"),
+        "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "POSTGRES_HOST": os.getenv("POSTGRES_HOST"),
+        "POSTGRES_PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -160,10 +160,10 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = "redis://localhost:6379"  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = "redis://redis:6379"  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = TIME_ZONE
